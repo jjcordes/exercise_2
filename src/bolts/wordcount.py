41e6e67947a139156f01404ad069e9d8cd2d2ee9
w205 <w205@ip-172-31-54-249.ec2.internal>
2017-03-25 05:59:16 +0000

    Finally got wordcount to work

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -11,6 +11,37 @@ class WordCounter(Bolt):
     def initialize(self, conf, ctx):
         self.counts = Counter()
 
+        conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+
+        #Create the Database
+        try:
+                # CREATE DATABASE can't run inside a transaction
+                conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+                cur = conn.cursor()
+                cur.execute("CREATE DATABASE tcount")
+                cur.close()
+                conn.close()
+        except:
+                self.log('Could not create tcount')
+                
+
+        #Connecting to tcount
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        #Create a Table
+        #The first step is to create a cursor.
+
+        try:
+                cur = conn.cursor()
+                cur.execute('''CREATE TABLE tweetwordcount (word TEXT PRIMARY KEY     NOT NULL, count INT     NOT NULL);''')
+                #conn.commit()
+                cur.close()
+                conn.close()
+        except:
+                self.log('Could not create tweetwordcount')
+
+
     def process(self, tup):
         word = tup.values[0]
 
@@ -21,52 +52,33 @@ class WordCounter(Bolt):
         # you need to create both the database and the table in advance.
         # Connect to the database
 
-        conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
-
-        #Create the Database
-
-        try:
-                # CREATE DATABASE can't run inside a transaction
-                conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-                cur = conn.cursor()
-                cur.execute("CREATE DATABASE tcount")
-                cur.close()
-                conn.close()
-        except:
-                print("Could not create tcount")
-
-        #Connecting to tcount
-
-        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-
-        #Create a Table
-        #The first step is to create a cursor.
-
-	try:
-        	cur = conn.cursor()
-        	cur.execute('''CREATE TABLE tweetwordcount (word TEXT PRIMARY KEY     NOT NULL, count INT     NOT NULL);''')
-        	conn.commit()
-	except:
-		print("Could not create tweetwordcount")
-
-
         # Increment the local count
         self.counts[word] += 1
 
-	try:
-	        #Insert
-        	conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-        	cur = conn.cursor()
-		cur.execute("INSERT INTO tweetwordcount (word,count) VALUES (%s, %s)", (word, self.counts[word]))
-        	conn.commit()
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        try:
+                #Insert
+                cur = conn.cursor()
+                cur.execute("INSERT INTO tweetwordcount (word,count) VALUES (%s, %s)", (word, self.counts[word]))
+                #conn.commit()
 
         except:
-		#Update
-		conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-		cur = conn.cursor()
-        	cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (self.counts[word], word))
-        	conn.commit()
+                #Update
+                #cur = conn.cursor()
+                cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (self.counts[word], word))
+                #conn.commit()
 
+        #cur = conn.cursor()
+        #cur.execute("SELECT word, count from tweetwordcount ORDER BY count DESC")
+        #records = cur.fetchall()
+        #for rec in records:
+        #        self.log('word = ' + rec[0] + ', count = ' + str(rec[1]))
+        #conn.commit()
+
+	cur.close()
+	conn.close()
 
         self.emit([word, self.counts[word]])
 
